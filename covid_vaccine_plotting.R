2943837453589c0703b41880018f8ba762ed3052
changadorji <71421770+changadorji@users.noreply.github.com>
2022-03-15 23:55:14 +0600

    Add files via upload

---
+++ covid_vaccine_plotting.R
@@ -0,0 +1,61 @@
+library(tidyverse)
+library(lubridate)
+library(geomtextpath)
+library(gridExtra)
+
+#read the file
+df <- read.csv('https://raw.githubusercontent.com/govex/COVID-19/master/data_tables/vaccine_data/global_data/time_series_covid19_vaccine_global.csv')
+
+country <- c('Guyana', 'Bhutan', 'Comoros', 'Fiji', 'Luxembourg')
+
+df_filtered <- df %>% 
+  filter(Country_Region %in% country) %>%
+  mutate(Date = as.Date(Date)) %>% 
+  filter(Date <= '2021-12-31') %>% 
+  mutate(month = format(Date, '%m'),
+         month1= format (Date, '%B'),
+         dose = Doses_admin/100000)
+
+
+# create table and themes
+table <- data.frame(Country=country,
+                    Population = c('786,552', '771,608', '869,601', '896,445', '625,978'))
+
+table <- table %>% 
+  arrange(Country)
+
+tt <- ttheme_default(core=list(bg_params=list(fill='black'),
+                               fg_params=list(col='white')),
+                     colhead=list(bg_params=list(fill='black'),
+                                  fg_params=list(col='white')),
+                     rowhead = list(fg_params=list(col='white')))
+
+
+
+
+#plotting
+df_plot <- df_filtered %>%
+  count(month, month1, Country_Region, wt=dose)
+
+df_plot %>%
+  ggplot(aes(as.numeric(month), n, colour=Country_Region))+
+  geom_textline(aes(label=Country_Region), hjust=.0001, size=6, linewidth=1.5)+
+  geom_text(data = subset(df_plot, month=='12'),
+                       aes(x=12, y=n, label=Country_Region), size=6, hjust=-.1)+
+  scale_x_continuous(limits = c(1,13), breaks = seq(1,12,1), labels = month.abb )+
+  labs (title = 'Total Dosage of COVID-19 Vaccine Administered in 2021',
+        y = 'Total Dosage (in thousand)',
+        x='',
+        caption = 'Source: John Hopkins University')+
+  theme(legend.position = 'none',
+        plot.background = element_rect(fill='black'),
+        panel.background = element_rect(fill='black'),
+        panel.grid = element_blank(),
+        axis.title = element_text(size = 18, colour = 'white'),
+        axis.text = element_text(size = 16, colour = 'red'),
+        plot.title = element_text(size=24, colour = 'white'),
+        plot.caption = element_text(size = 13, colour = 'white', face='italic'),
+        axis.title.y = element_text(margin = margin(t = 0, r = 25, b = 0, l = 0)))+
+  annotation_custom(tableGrob(table, theme=tt), xmin = .5, xmax=5, ymin=180, ymax = 350)
+
+
